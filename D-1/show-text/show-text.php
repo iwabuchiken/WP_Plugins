67141369e2af52395c53b2d203ee1cd1ea52a1a5
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-08-27 21:18:46 +0900

    m
    v-1.0
    ---
    Initial

---
+++ D-1/show-text/show-text.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+
+Plugin Name: Show Text
+
+Plugin URI: http://www.example.com/plugin
+
+Description: テキストを表示するだけのプラグイン・・・だ！
+
+Author: my name
+
+Version: 0.1
+
+Author URI: http://www.example.com
+
+*/
+
+ 
+
+function show_text() {
+
+    echo 'おはようございます。';
+
+}
